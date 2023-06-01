a1cca53debbac75076bde049b6704b9ebe14fb55
Blue Groneman <hello@bluedojo.dev>
2023-06-01 20:36:32 +0000

    WIP

--- src/components/global/Navigation.svelte
+++ src/components/global/Navigation.svelte
@@ -14,42 +14,96 @@
       name: "Stories",
       link: "/stories",
     },
+    {
+      name: "Events",
+      link: "/events"
+    },
   ];
 
   let navToggled = false;
 </script>
-<div class="bg-t-teal shadow-md">
-    <div class="max-w-7xl mx-auto p-6 sm:px-6">
-      <nav
-        class="relative flex items-center justify-between sm:h-10 md:justify-center"
-        aria-label="Global"
-      >
-        <div class="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
-          <div class="flex items-center justify-between w-full md:w-auto">
-            <a href="/">
-              <span class="sr-only">Almost home</span>
-              <img
-                src={logo}
-                class="h-8 w-auto sm:h-10"
-                alt="logo - heart with a house in the center"
-              />
-            </a>
-          </div>
+
+<nav class="bg-gray-800">
+  <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <div class="flex h-16 items-center justify-between">
+      <div class="flex items-center">
+        <div class="flex-shrink-0">
+            <img
+              src={logo}
+              class="h-8 w-auto sm:h-10"
+              alt="logo - heart with a house in the center"
+            />
         </div>
-        <div class="md:flex md:space-x-10">
+        <div class="hidden sm:ml-6 sm:block">
+          <div class="flex space-x-4">
+            <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
           {#each navItems as item}
             <a
               href={item.link}
-              class="font-semibold text-white hover:text-sky-100 sm:text-sm md:text-lg tracking-wide"
+              class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
             >
               {item.name}
             </a>
-          {/each}
+          {/each} 
+          </div>
+        </div>
+      </div>
+      <div class="hidden sm:ml-6 sm:block">
+        <div class="flex items-center">
+          <button type="button" class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+            <PrimaryBtn link="/donate/">Donate</PrimaryBtn>
+          </button>
+        </div>
+      </div>
+      <div class="-mr-2 flex sm:hidden">
+        <!-- Mobile menu button -->
+        <button type="button" class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+          <span class="sr-only">Open main menu</span>
+          <!--
+            Icon when menu is closed.
 
-        </div>
-        <div class="justify-self-end flex items-center flex-1 md:absolute md:inset-y-0 md:right-0">
-          <PrimaryBtn link="/donate/">Donate</PrimaryBtn>
-        </div>
-      </nav>
+            Menu open: "hidden", Menu closed: "block"
+          -->
+          <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          </svg>
+          <!--
+            Icon when menu is open.
+
+            Menu open: "block", Menu closed: "hidden"
+          -->
+          <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
+      </div>
     </div>
   </div>
+
+  <!-- Mobile menu, show/hide based on menu state. -->
+  <div class="sm:hidden" id="mobile-menu">
+    <div class="space-y-1 px-2 pb-3 pt-2">
+      {#each navItems as item}
+          <a
+            href={item.link}
+            class="font-semibold text-white hover:text-sky-100 sm:text-sm md:text-lg tracking-wide"
+          >
+            {item.name}
+          </a>
+        {/each}
+      <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+    </div>
+    <div class="border-t border-gray-700 pb-3 pt-4">
+      <div class="mt-3 space-y-1 px-2">
+        {#each navItems as item}
+          <a
+            href={item.link}
+            class="font-semibold text-white hover:text-sky-100 sm:text-sm md:text-lg tracking-wide"
+          >
+            {item.name}
+          </a>
+        {/each}
+      </div>
+    </div>
+  </div>
+</nav>
