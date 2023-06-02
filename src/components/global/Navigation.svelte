a1fd92605b8b51467d8b9c6607abfce241774c1b
Blue Groneman <hello@bluedojo.dev>
2023-06-02 23:49:39 +0000

    navigation styling

--- src/components/global/Navigation.svelte
+++ src/components/global/Navigation.svelte
@@ -1,6 +1,7 @@
 <script>
   import logo from '../../images/almost_home_white.png';
   import PrimaryBtn from "./PrimaryBtn.svelte";
+  import Hamburger from './Hamburger.svelte';
   let navItems = [
     {
       name: "About",
@@ -23,7 +24,7 @@
   let navToggled = false;
 </script>
 
-<nav class="bg-gray-800">
+<nav class="bg-t-teal">
   <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
     <div class="flex h-16 items-center justify-between">
       <div class="flex items-center">
@@ -34,13 +35,12 @@
               alt="logo - heart with a house in the center"
             />
         </div>
-        <div class="hidden sm:ml-6 sm:block">
+        <div class="sm:ml-6 sm:block">
           <div class="flex space-x-4">
-            <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
           {#each navItems as item}
             <a
               href={item.link}
-              class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+              class="rounded-md px-3 py-2 text-sm font-bold text-white hover:text-white"
             >
               {item.name}
             </a>
@@ -50,38 +50,20 @@
       </div>
       <div class="hidden sm:ml-6 sm:block">
         <div class="flex items-center">
-          <button type="button" class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-            <PrimaryBtn link="/donate/">Donate</PrimaryBtn>
+          <button type="button" class="rounded-full p-1 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+            <PrimaryBtn class="uppercase" link="/donate/">Donate</PrimaryBtn>
           </button>
         </div>
       </div>
       <div class="-mr-2 flex sm:hidden">
-        <!-- Mobile menu button -->
-        <button type="button" class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-          <span class="sr-only">Open main menu</span>
-          <!--
-            Icon when menu is closed.
-
-            Menu open: "hidden", Menu closed: "block"
-          -->
-          <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-          </svg>
-          <!--
-            Icon when menu is open.
-
-            Menu open: "block", Menu closed: "hidden"
-          -->
-          <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-          </svg>
-        </button>
+        <Hamburger bind:open={navToggled}/>
       </div>
     </div>
   </div>
 
   <!-- Mobile menu, show/hide based on menu state. -->
-  <div class="sm:hidden" id="mobile-menu">
+  {#if navToggled}
+  <div id="mobile-menu">
     <div class="space-y-1 px-2 pb-3 pt-2">
       {#each navItems as item}
           <a
@@ -91,19 +73,7 @@
             {item.name}
           </a>
         {/each}
-      <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-    </div>
-    <div class="border-t border-gray-700 pb-3 pt-4">
-      <div class="mt-3 space-y-1 px-2">
-        {#each navItems as item}
-          <a
-            href={item.link}
-            class="font-semibold text-white hover:text-sky-100 sm:text-sm md:text-lg tracking-wide"
-          >
-            {item.name}
-          </a>
-        {/each}
-      </div>
     </div>
   </div>
+  {/if}
 </nav>
