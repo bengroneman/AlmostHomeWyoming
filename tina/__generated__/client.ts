8c3c8a4264e6c10dc2d15cb669e67a483fc58f82
bengroneman <ben.groneman@hey.com>
2023-10-02 16:10:04 -0600

    remove generated folder

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '959ebe839431546a69e09a87391aa2937e224763', queries });
-export default client;
-  
