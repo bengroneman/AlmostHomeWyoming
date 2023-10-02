f2491c2b00fa20821217255da8dca4f882a1b295
bengroneman <ben.groneman@hey.com>
2023-10-02 06:26:19 -0600

    inital checkin

---
+++ tina/config.ts
@@ -0,0 +1,88 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: process.env.CLIENT_ID, // Get this from tina.io
+  token: process.env.TOKEN, // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "subTitle",
+            label: "Sub Title",
+            required: true,
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description",
+            required: false,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+          {
+            type: 'image',
+            label: 'Hero image',
+            name: 'imgSrc',
+          },
+        ],
+      },
+      {
+        name: "page",
+        label: "Pages",
+        path: "content/pages",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+          {
+            type: "string",
+            name: "subTitle",
+            label: "Sub Title",
+            required: false,
+          },
+        ],
+      },
+    ],
+  },
+});
