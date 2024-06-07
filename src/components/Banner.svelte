3a8951b39c7b2878edd8a9020a6c9cb5fa75af0b
bengroneman <bluegroneman10@gmail.com>
2024-06-06 19:37:54 -0600

    updated content for Challenge for Charities

--- src/components/Banner.svelte
+++ src/components/Banner.svelte
@@ -1,7 +1,7 @@
-<div class="fixed bottom-0 flex items-center gap-x-6 bg-indigo-600 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+<div class="fixed bottom-0 w-full flex text-center items-center gap-x-6 bg-indigo-600 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
   <p class="text-sm leading-6 text-white">
     <a href="#c4c">
-      <strong class="font-semibold">Challenge for Charities 2023</strong><svg viewBox="0 0 2 2" class="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true"><circle cx="1" cy="1" r="1" /></svg>Join us this summer, help us get the match
+      <strong class="font-semibold text-center">Challenge for Charities 2024</strong><svg viewBox="0 0 2 2" class="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true"><circle cx="1" cy="1" r="1" /></svg>Join us this summer, help us give back to those in need
     </a>
   </p>
 </div>
