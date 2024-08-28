2eeb0ace1326dab48929a79dec1d7f660bd6d598
bengroneman <bluegroneman10@gmail.com>
2024-08-27 18:50:49 -0600

    wcf - lcf

--- src/components/Banner.svelte
+++ src/components/Banner.svelte
@@ -1,7 +1,8 @@
 <div class="fixed bottom-0 w-full flex text-center items-center gap-x-6 bg-indigo-600 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
   <p class="text-sm leading-6 text-white">
     <a href="#c4c">
-      <strong class="font-semibold text-center">Challenge for Charities 2024</strong><svg viewBox="0 0 2 2" class="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true"><circle cx="1" cy="1" r="1" /></svg> was a great success, thank you all for helping to support our mission. A huge shoutout to the Wyoming Community Foundation for putting this together!
+      <strong class="font-semibold text-center">Challenge for Charities 2024</strong> was a great success, thank you all for helping to support our mission.
+      A huge shoutout to the Lander Community Foundation for putting this together!
     </a>
   </p>
 </div>
